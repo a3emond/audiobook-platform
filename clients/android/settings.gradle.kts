9568c98aa55cfc3866027e9c95e1b9e7c44b378a
a3emond <a3emond@gmail.com>
2026-04-11 15:32:51 -0400

    chore: remove obsolete Android and Apple client project files

--- clients/android/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "audiobook-android"
-
-include(":app")
-include(":core-network")
-include(":core-auth")
-include(":core-storage")
-include(":core-realtime")
-include(":core-domain")
-include(":core-data")
